c3daa560894d3911c469f2e8c4b7bf02e79b25ee
clayton24242 <42525534+clayton24242@users.noreply.github.com>
2021-04-10 13:58:09 -0400

    Create Dealers_working_during_losses - Copy.sh

---
+++ Linux/Dealers_working_during_losses - Copy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh   
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0310_Dealer_schedule |  grep '05:00:00 AM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0310_Dealer_schedule |  grep '08:00:00 AM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0310_Dealer_schedule |  grep '02:00:00 PM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0310_Dealer_schedule |  grep '08:00:00 PM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0310_Dealer_schedule |  grep '11:00:00 PM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0312_Dealer_schedule |  grep '05:00:00 AM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0312_Dealer_schedule |  grep '08:00:00 AM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0312_Dealer_schedule |  grep '02:00:00 PM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0312_Dealer_schedule |  grep '08:00:00 PM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0312_Dealer_schedule |  grep '11:00:00 PM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0315_Dealer_schedule |  grep '05:00:00 AM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0315_Dealer_schedule |  grep '08:00:00 AM' >> Notes_Dealer_Analysis
+awk '{print $1 " " $2 "::::" $5 "  " $6 }' 0315_Dealer_schedule |  grep '02:00:00 PM' >> Notes_Dealer_Analysis
